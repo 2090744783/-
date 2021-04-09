e0ad0dbed6bcedb37b996724656f748fa31e3995
2090744783 <81237185+2090744783@users.noreply.github.com>
2021-04-09 09:29:49 -0500

    Add files via upload

---
+++ for循环.cpp
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+//	int i = 1; //初始化
+//	while (i <= 10)//判断条件
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d\n", i);
+//		i++; //调整
+//	}
+
+	/*int i=0;
+	for (i = 1; i <= 10; i++)
+	{
+		if (i == 5)
+			continue;
+		printf("%d\n", i);
+	}*/
+
+	/*int i = 0;
+	for (i = 1; i <= 10; i++)
+	{
+		if (i == 5)
+			printf("haha\n");
+	printf("hehr\n", i);
+	}*/
+		
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			printf("呵呵");
+		}
+	}
+return 0;
+}

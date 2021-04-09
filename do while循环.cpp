1706ccc86294347c9d117a7bc332e321141c8f08
2090744783 <81237185+2090744783@users.noreply.github.com>
2021-04-09 09:30:51 -0500

    Add files via upload

---
+++ do while循环.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	// do  
+	//循环语句;
+	//while（表达式）;
+
+	int i = 1;
+	do
+	{
+		printf("%d ", i);
+		i++;
+	}
+	
+	while (i <= 10);
+
+
+	
+return 0;
+}

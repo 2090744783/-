3b2a0eb8d8201999f4f10250121358aaf49b1a38
2090744783 <81237185+2090744783@users.noreply.github.com>
2021-04-10 05:02:02 -0500

    Add files via upload

---
+++ 模拟用户登录情景.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i = 0;
+	char password[20] = { 0 };
+	for (i = 0; i < 3; i++)
+	{
+		printf("请输入密码:>");
+		scanf("%s", &password);
+		if (strcmp(password, "123456s") == 0)
+		{
+			printf("登陆成功！");
+			break;
+		}
+		else
+		{
+			printf("密码错误!");
+		}
+	}
+	if (i == 3)
+	{
+		printf("三次输入错误，退出程序");
+	}
+return 0;
+}

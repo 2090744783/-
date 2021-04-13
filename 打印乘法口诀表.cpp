7c07d5702fd410e873f205903377c3f459fb8408
2090744783 <81237185+2090744783@users.noreply.github.com>
2021-04-13 08:32:13 -0500

    Add files via upload

---
+++ 打印乘法口诀表.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	for (i = 1; i <= 9; i++)
+	{
+		//´òÓ¡Ò»ÐÐ
+		int j = 1;
+		for ( j = 1; j <= i; j++)
+		{
+			printf("%d*%d=%-3d",i,j,i*j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
